18d6320eb1a5ada4b1a9f1eb47f41068921cddc3
soni lamkaj <soni.lamkaj@fullscreendigital.ro>
2020-01-13 16:38:34 +0100

    created auth service and user model

---
+++ auth-service/config/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  development: {
+    host: "localhost",
+    username: "root",
+    password: "123456789",
+    port: 3310,
+    database: "Users",
+    dialect: "mysql"
+  },
+  production : {
+    host: "localhost",
+    username: "root",
+    password: "123456789",
+    port: 3310,
+    database: "Users",
+    dialect: "mysql"
+  }
+};
